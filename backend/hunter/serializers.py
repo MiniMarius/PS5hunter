a217699053cf9496e5988edbedb8571d61c3ec48
MiniMarius <seffermarius@gmail.com>
2023-02-26 22:13:09 +0100

    added website to be serialized together with products

--- backend/hunter/serializers.py
+++ backend/hunter/serializers.py
@@ -2,14 +2,16 @@ from rest_framework import serializers
 from .models import Product
 from .models import Website
 from .models import TagData
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'availability', 'url', 'price', 'dateCreated', 'dateUpdated', 'website')
+
 class WebsiteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Website
         fields = ('id','name', 'url', 'relatedTagData')
+class ProductSerializer(serializers.ModelSerializer):
+    website = WebsiteSerializer()
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'availability', 'url', 'price', 'dateCreated', 'dateUpdated', 'website')
 class TagDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = TagData
